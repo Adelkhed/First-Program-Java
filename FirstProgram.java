d1f52008a09102224641d21b6643ae24ce9058e1
Adel Khedhiri <khedhiri.adel@gmail.com>
2024-11-23 13:52:48 +0100

    first commit

---
+++ FirstProgram.java
@@ -0,0 +1,11 @@
+/**
+ * FirstProgram
+ */
+public class FirstProgram {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Adel Khedhiri ");
+        System.out.println("I'm 45 years old");
+        System.out.println("My home town is Sousse, Tunisia ");
+    }
+}
